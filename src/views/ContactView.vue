b923216e182966441a893242178b465ef3c9d453
Olloshukur6092 <matnazarovolloshukur3107223@gmail.com>
2023-03-04 15:12:30 +0500

    Xatolik tuzatildi

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <section id="hero-section">
-      <div class="hero-img">
+    <section id="contact-section">
+      <div class="contact-img">
         <div class="container">
           <div class="row">
             <div class="col-md-4">
@@ -13,7 +13,8 @@
             <div class="col-md-4">
               <div class="about-text">
                 <p class="text-white">
-                    Biz bilan bog‘lanish mumkin bo‘lgan barcha ma’lumotlarni shu bo‘limdan olishingiz mumkin
+                  Biz bilan bog‘lanish mumkin bo‘lgan barcha ma’lumotlarni shu
+                  bo‘limdan olishingiz mumkin
                 </p>
               </div>
             </div>
@@ -26,7 +27,7 @@
             <div class="col-md-4">
               <div class="card card-body p-4 border-0">
                 <div class="card-title border-bottom p-3">
-                  <img class="me-4" src="../assets/Frames/Frame.png" alt="">
+                  <img class="me-4" src="../assets/Frames/Frame.png" alt="" />
                   Telefon raqam
                 </div>
                 <div class="card-subtitle pt-3 ps-3">
@@ -36,9 +37,9 @@
               </div>
             </div>
             <div class="col-md-4">
-                <div class="card card-body p-4 border-0">
+              <div class="card card-body p-4 border-0">
                 <div class="card-title border-bottom p-3">
-                  <img class="me-4" src="../assets/Frames/Frame1.png" alt="">
+                  <img class="me-4" src="../assets/Frames/Frame1.png" alt="" />
                   Email pochta
                 </div>
                 <div class="card-subtitle pt-3 ps-3">
@@ -48,9 +49,9 @@
               </div>
             </div>
             <div class="col-md-4">
-                <div class="card card-body p-4 border-0">
+              <div class="card card-body p-4 border-0">
                 <div class="card-title border-bottom p-3">
-                  <img class="me-4" src="../assets/Frames/Frame2.png" alt="">
+                  <img class="me-4" src="../assets/Frames/Frame2.png" alt="" />
                   Manzil
                 </div>
                 <div class="card-subtitle pt-3 ps-3">
@@ -64,24 +65,31 @@
       </div>
     </section>
     <section id="location" class="mb-4">
-        <div class="container">
-            <div class="col-md-12">
-            <img class="location" src="../assets/Rectangle15.png" alt="">
-            </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <img
+              class="location img-fluid w-100"
+              src="../assets/Rectangle15.png"
+              alt=""
+            />
+          </div>
         </div>
+      </div>
     </section>
-    <section id="form" class="mt-5 ">
-        <div class="container">
-            <div class="team">
+    <section id="form" class="mt-5">
+      <div class="container">
+        <div class="team">
           <div class="row">
             <div class="col-md-4 me-5">
-              <div class="card card-body border-0 card-center-text">
+              <div class="card card-body border-0">
                 <div class="card-title">
                   <h3>O‘zimiz siz bilan bog‘lanamiz</h3>
                 </div>
                 <div class="card-subtitle">
                   <p>
-                    Berilgan hududlarni to‘ldiring, savol yoki taklifingiz bo‘lsa ham yozavering, siz bilan o‘zimiz bog‘lanamiz.
+                    Berilgan hududlarni to‘ldiring, savol yoki taklifingiz
+                    bo‘lsa ham yozavering, siz bilan o‘zimiz bog‘lanamiz.
                   </p>
                 </div>
               </div>
@@ -89,23 +97,34 @@
             <div class="col-md-7 ms-5">
               <form action="">
                 <div class="row mb-2">
-                    <div class="col">
-                        <input type="text" class="form-control" placeholder="Ism">
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" placeholder="Telefon raqam">
-                    </div>
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Ism" />
+                  </div>
+                  <div class="col">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Telefon raqam"
+                    />
+                  </div>
                 </div>
                 <div class="txt-area">
-                    <div class="col-md-12">
-                    <textarea class="form-control" name="" id="" cols="32" rows="8" placeholder="Savolingiz yoki murojaatingiz bo‘lsa yozing..."></textarea>
-                    </div>
+                  <div class="col-md-12">
+                    <textarea
+                      class="form-control"
+                      name=""
+                      id=""
+                      cols="32"
+                      rows="8"
+                      placeholder="Savolingiz yoki murojaatingiz bo‘lsa yozing..."
+                    ></textarea>
+                  </div>
                 </div>
               </form>
             </div>
           </div>
         </div>
-        </div>
+      </div>
     </section>
   </div>
 </template>
